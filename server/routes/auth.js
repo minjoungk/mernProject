add56b334c7b05d3c684bb28d3f0edb9254a420a
MinJoung Kim <minjoungk@gmail.com>
2022-04-12 23:55:14 -0700

    create another branch

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 //import express from "express";
-
+//checking branch
+//Need to work the below code out.
 //import {register} from '../controllers/auth';
 
 const router = express.Router();
