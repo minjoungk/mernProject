d8f6fac5c360814dc3f1a65cc6b5b9643a2f4143
MinJoung Kim <minjoungk@gmail.com>
2022-04-13 00:00:59 -0700

    create branch Kim

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 //import express from "express";
 //checking branch
+//from "Kim" branch
 //Need to work the below code out.
 //import {register} from '../controllers/auth';
 
